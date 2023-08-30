75faadd813817e176ce49dd8afa0b71f221f6636
Sadman95 <mail.sakib.1995@gmail.com>
2023-08-30 17:58:30 +0600

    env config updated

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 export const envConfig = {
   apiUrl:
-    import.meta.env.NODE_ENV === 'production'
+    import.meta.env.NODE_ENV !== 'development'
       ? import.meta.env.VITE_APP_LIVE_URL
       : import.meta.env.VITE_APP_LOCAL_URL,
 };
