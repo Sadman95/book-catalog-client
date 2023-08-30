81952c34101dd04ec71e06408b7eebf309bce4c8
Sadman95 <mail.sakib.1995@gmail.com>
2023-08-30 23:13:41 +0600

    deploy issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-react-router',
+  base: '/',
   server: {
     port: 3000,
   },
